22103f2a7767ef9c6c218b4c805db7ac88d191d0
Loczek <rekowski.krzysztof@gmail.com>
2020-03-14 19:58:34 +0100

    zabawa z statics 4

--- StronaMaracza/settings.py
+++ StronaMaracza/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR,'var', 'static')
 
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
